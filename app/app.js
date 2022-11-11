4ea43e6046fab81ef7e635e014e63eb91fba6d75
Mark Thompson <mthompson@bitovi.com>
2022-11-10 21:48:35 -0800

    removed unused import

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 import StacheElement from "can-stache-element";
 import view from "./app.stache";
 import "./app.css";
-import "./my-component/my-component";
 
 class App extends StacheElement {
     static view = view;
