273bd7631243948b6d49e75a496c4eb4508e7721
Mark Thompson <mthompson@bitovi.com>
2022-11-10 21:47:14 -0800

    added my-component

---
+++ app/my-component/my-component.js
@@ -0,0 +1,9 @@
+import StacheElement from "can-stache-element";
+import view from "./my-component.stache";
+import "./my-component.css";
+
+class MyComponent extends StacheElement {
+    static view = view;
+}
+
+customElements.define('can-my-component', MyComponent)
