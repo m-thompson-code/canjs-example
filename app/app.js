0d24b1a01b2364a8e84e88ab4d0d80dc9aa9d2ad
Mark Thompson <mthompson@bitovi.com>
2022-11-10 21:37:54 -0800

    created app

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import StacheElement from "can-stache-element";
+import view from "./app.stache";
+import "./app.css";
+import "./my-component/my-component";
+
+class App extends StacheElement {
+    static view = view;
+}
+
+customElements.define('can-app', App)
